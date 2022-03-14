cc08a33a6cca163f0e2342f3432a1e2fcde7b11a
bielnavarro <bielnavarrothau@gmail.com>
2022-03-14 12:31:20 +0100

    es mou el eseba i es para quna yo li dic
    nose

--- Godot.gd
+++ Godot.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends Area2D
 
 
 var velocitat := 300
@@ -6,14 +6,29 @@ var direccio = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rotation_degrees = 90
+	rotation_degrees = 0
 	position = Vector2(500,300)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += direccio * velocitat * delta
-	
 	if Input.is_action_pressed("mou dreta"):
-		direccio = Vector2(1,0)
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed("mou esquerra"):
+		direccio += Vector2.LEFT
+	if Input.is_action_pressed("mou amunt"):
+		direccio += Vector2.UP
+	if Input.is_action_pressed("mou avall"):
+		direccio += Vector2.DOWN
+		
+	position += direccio.normalized() * velocitat * delta
 	
+	direccio = Vector2(0,0)
+
+
+func _on_Personatge_area_entered(area):
+	modulate = Color(1,0,0)
+
+
+func _on_Personatge_area_exited(area):
+	modulate = Color(1,1,1)
